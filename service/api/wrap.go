c94f56fd5791aff229c9debdc6312f62197066cc
zlyuancn <zlyuancn@163.com>
2020-12-25 10:27:01 +0800

    修改api.Wrap,将包装中间件单独写函数,提供WriteToCtx

--- service/api/wrap.go
+++ service/api/wrap.go
@@ -9,6 +9,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/kataras/iris/v12"
 	iris_context "github.com/kataras/iris/v12/context"
 
@@ -28,49 +30,64 @@ type Response struct {
 }
 
 // 包装处理程序
-//
-// 如果是中间件返回nil才能继续下一步, 非nil值表示拦截, 并将结果处理后返回给客户端
-func Wrap(handler Handler, isMiddleware ...bool) iris.Handler {
-	isProduction := !component.GlobalComponent().Config().Frame.Debug
-	showDetailedErrorOfProduction := component.GlobalComponent().Config().ApiService.ShowDetailedErrorOfProduction
+func Wrap(handler Handler) iris.Handler {
 	return func(irisCtx *iris_context.Context) {
-		ctx := makeContext(irisCtx)
-		result := handler(ctx)
+		ctx := makeContext(irisCtx) // 构建上下文
+		result := handler(ctx)      // 处理
+		WriteToCtx(ctx, result)     // 写入结果
+		ctx.StopExecution()         // 停止调用链
+	}
+}
 
-		if result == nil && len(isMiddleware) > 0 && isMiddleware[0] { // 返回了nil且为中间件
+// 包装中间件, 只有返回nil才能继续调用链, 非nil值表示拦截, 并将结果处理后返回给客户端
+func WrapMiddleware(middleware Handler) iris.Handler {
+	return func(irisCtx *iris_context.Context) {
+		ctx := makeContext(irisCtx) // 构建上下文
+		result := middleware(ctx)   // 处理
+		if result == nil {          // 返回nil继续调用链
 			ctx.Next()
 			return
 		}
 
-		if err, ok := result.(error); ok {
-			ctx.Values().Set("error", err)
-			code, message := decodeErr(err)
-			if !isProduction || showDetailedErrorOfProduction {
-				message = err.Error()
-			}
-			_, _ = ctx.JSON(Response{
-				ErrCode: code,
-				ErrMsg:  message,
-			})
-			ctx.StopExecution()
-			return
-		}
+		WriteToCtx(ctx, result) // 写入结果
+		ctx.StopExecution()     // 停止调用链
+	}
+}
 
-		switch v := result.(type) {
-		case []byte:
-			ctx.Values().Set("result", "bytes")
-			_, _ = ctx.Write(v)
-		case *[]byte:
-			ctx.Values().Set("result", "bytes")
-			_, _ = ctx.Write(*v)
-		default:
-			ctx.Values().Set("result", result)
-			_, _ = ctx.JSON(Response{
-				ErrCode: 0,
-				ErrMsg:  "ok",
-				Data:    result,
-			})
+// 写入数据到ctx
+//
+// 如果返回bytes会直接返回给客户端
+// 返回其它值会经过处理后再返回给客户端
+func WriteToCtx(ctx *Context, result interface{}) {
+	if err, ok := result.(error); ok {
+		ctx.Values().Set("error", err)
+		code, message := decodeErr(err)
+
+		isProduction := !component.GlobalComponent().Config().Frame.Debug
+		showDetailedErrorOfProduction := component.GlobalComponent().Config().ApiService.ShowDetailedErrorOfProduction
+		if !isProduction || showDetailedErrorOfProduction {
+			message = err.Error()
 		}
-		ctx.StopExecution()
+		_, _ = ctx.JSON(Response{
+			ErrCode: code,
+			ErrMsg:  message,
+		})
+		return
+	}
+
+	switch v := result.(type) {
+	case []byte: // 直接写入
+		ctx.Values().Set("result", fmt.Sprintf("bytes<len=%d>", len(v)))
+		_, _ = ctx.Write(v)
+	case *[]byte: // 直接写入
+		ctx.Values().Set("result", fmt.Sprintf("bytes<len=%d>", len(*v)))
+		_, _ = ctx.Write(*v)
+	default:
+		ctx.Values().Set("result", result)
+		_, _ = ctx.JSON(Response{
+			ErrCode: OK.Code,
+			ErrMsg:  OK.Message,
+			Data:    result,
+		})
 	}
 }
